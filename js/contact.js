1939fc80de244fff2703f3a9a0cddbd2c3fd7e7b
Kyra Weber <kyra.m.weber@gmail.com>
2014-07-27 09:24:24 -0600

    get

--- js/contact.js
+++ js/contact.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
  
 $('#submit').click(function(){
  
-$.post("contact.php", $("#contact").serialize(),  function(response) {
+$.get("contact.php", $("#contact").serialize(),  function(response) {
 $('#success').html(response);
 //$('#success').hide('slow');
 });
